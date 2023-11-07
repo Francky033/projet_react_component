b9c7e3e92edac3b0f19fba20be823e0d04f7aeb6
Franck NGONG <franck.ngong@lapiscine.pro>
2023-11-07 17:28:06 +0100

    poney

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@
 import Header from "./Header";
 import Home from "./Home";
 import Footer from "./Footer";
+import './App.css'
 
 function App() {
   return (
-    <div>
+    <>
       <Header/>
       <Home/>
       <Footer/>
-    </div>
+    </>
   );
 }
 
--- src/Product.js
+++ src/Product.js
@@ -1,24 +1,27 @@
 function Product() {
     return(
         <main>
-            <div>
+            <article>
             <h3>Poneyglote</h3>
+            <img src='https://i.pinimg.com/originals/32/46/7c/32467ceafb70c2e843062fad739d3860.jpg'></img>
             <p>Poney rare Albinosse capable d'aller jusqu'a au moins 15km/h </p>
             <p>$5 000</p>
-        </div>
+        </article>
 
-           <div>
+           <article>
             <h3>Poneygliffe</h3>
+            <img src='https://i.pinimg.com/originals/32/46/7c/32467ceafb70c2e843062fad739d3860.jpg'></img>
             <p>Poney rare Unijambiste capable d'aller jusqu'a au moins 10km/h </p>
             <p>$7 000</p>
-        </div>
+        </article>
 
         
-        <div>
+        <article>
             <h3>Poneygreffe</h3>
+            <img src='https://i.pinimg.com/originals/32/46/7c/32467ceafb70c2e843062fad739d3860.jpg'></img>
             <p>Poney rare Albinosse et Unijambiste capable d'aller jusqu'a au moins 5km/h </p>
             <p>$9 000</p>
-        </div>
+        </article>
 
         </main>
         
