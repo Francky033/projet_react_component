943084c4188d9ae862e3a392f49996463a217e13
Franck NGONG <franck.ngong@lapiscine.pro>
2023-11-09 14:17:04 +0100

    browserRout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 
-import Header from "./Header";
 import Home from "./Home";
-import Footer from "./Footer";
-import './App.scss'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Contact from "./Contact";
 
 function App() {
   return (
-    <>
-      <Header/>
-      <Home/>
-      <Footer/>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
